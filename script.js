63413cacac370b094b8fc283c70dcae96b3c4bac
unknown <lika_smorgun@outlook.com>
2022-04-10 14:52:08 +0300

    picture1

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+//ПЕРЕМЕННЫЕ
+
+//var leftBorderWidth = 1;
+//let second = 2;
+//const pi = 3.14;
+
+//console.log(leftBorderWidth);
+
+/////////////////////////
+
+// Классфикация типов даннх в JS
+
+var number = 5;
+var string = "Hello!";
+var sum = Symbol();
+var boollean = true;
+null;
+undefined;
+var obj = {}; //объект
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+console.log(4/0);
+console.log('string'*9);
+
+//Массив
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[2]);
+
+
+
+
+
+
+
+
